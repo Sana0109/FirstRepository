e3e986027ba6980f06e622ba5814ee9d619eb247
Oksana_Vasilkova <vasilkova010972@gmail.com>
2022-10-14 17:21:42 +0300

    first progekt

---
+++ src/com/animal/Dog.java
@@ -0,0 +1,13 @@
+package com.animal;
+
+public class Dog {
+    private  String firstName;
+    private  String secondName;
+    public static int id = 0;
+    public int idTest = 0;
+
+    public Dog() {
+        id++;
+        idTest++;
+    }
+}
---
+++ src/com/animal/Main.java
@@ -0,0 +1,15 @@
+package com.animal;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+
+        Dog firstName = new Dog();
+        Dog secondName = new Dog();
+        Dog LectureThird = new Dog();
+
+
+        System.out.println(Dog.id);
+    }
+}
